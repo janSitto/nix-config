dd18aab86606bedd285ed7244ed5387d27a8f39c
janSitto <jvtrschmitt404@gmail.com>
2025-08-08 09:15:05 -0300

    tailscale

--- modules/server/tailscale-server.nix
+++ modules/server/tailscale-server.nix
@@ -1,17 +1,21 @@
 {pkgs, ...}: {
     services.tailscale = {
         enable = true;
-        useRoutingFeatures = "both";
+        useRoutingFeatures = "both"
     };
     networking = {
         firewall = {
             trustedInterfaces = [ "tailscale0" ];
-            allowForwardFrom = [ "tailscale0" ];
             allowedUDPPorts = [ 41641 ];
+            extraCommands = ''
+                iptables -A nixos-fw-forward -i tailscale0 -j ACCEPT
+                ip6tables -A nixos-fw-forward -i tailscale0 -j ACCEPT
+            '';
         };
         nat = {
             enable = true;
             internalInterfaces = [ "tailscale0" ];
+            externalInterface = config.networking.defaultGatewayInterface;
         };
         sysctl = {
             "net.ipv4.ip_forward" = true;
