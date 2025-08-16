9fbc07ba9ea5edd574fd0b5917a1dca9ad6b646f
janSitto <jvtrschmitt404@gmail.com>
2025-08-16 14:02:58 -0300

    audio fix

--- system/pipewire.nix
+++ system/pipewire.nix
@@ -6,5 +6,10 @@
       pulse.enable = true;
       jack.enable = true;
     };
-    environment.systemPackages = with pkgs; [ pavucontrol ];
+    environment.systemPackages = with pkgs; [ 
+      pavucontrol 
+      wireplumber
+      libcanberra-gtk3
+      noise-suppression-for-voice
+    ];
 }
