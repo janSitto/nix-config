8abf668b7fae7c3e916e0e1aef06d553f6c305b7
janSitto <jvtrschmitt404@gmail.com>
2025-08-19 10:54:05 -0300

    server iptables fix

--- modules/server/tailscale-server.nix
+++ modules/server/tailscale-server.nix
@@ -13,10 +13,10 @@
         firewall = {
             trustedInterfaces = [ "tailscale0" ];
             allowedUDPPorts = [ 41641 ];
-            extraCommands = ''
-                iptables -A nixos-fw-forward -i tailscale0 -j ACCEPT
-                ip6tables -A nixos-fw-forward -i tailscale0 -j ACCEPT
-            '';
+            #extraCommands = ''
+            #    iptables -A nixos-fw-forward -i tailscale0 -j ACCEPT
+            #    ip6tables -A nixos-fw-forward -i tailscale0 -j ACCEPT
+            #'';
         };
         nat = {
             enable = true;
