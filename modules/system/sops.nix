4db0343332e8a3ce0f308145e3ad723c5a04d1a8
janSitto <jvtrschmitt404@gmail.com>
2025-11-16 10:43:28 -0300

    stuff

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -1,4 +1,5 @@
 {pkgs, username, ...}: {
+    environment.systemPackages = with pkgs; [ sops ];
     sops = {
         age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
         defaultSopsFile = ../../secrets/secrets.yaml;
