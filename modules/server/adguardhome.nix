609cf82b79eec15732f75c8f86a6c4eaf49637db
janSitto <jvtrschmitt404@gmail.com>
2025-12-16 01:38:41 -0300

    adguardhome

--- modules/server/adguardhome.nix
+++ modules/server/adguardhome.nix
@@ -40,7 +40,8 @@
             ];
         };
     }; 
-    systemd.services. adguardhome. serviceConfig.ExecStartPre = lib.mkBefore [
-        "+${pkgs.bash}/bin/bash -c 'PASSWORD=$(cat ${config.sops.secrets.adguardhome-password.path}) && ${pkgs.yq-go}/bin/yq eval \". users = [{\\\"name\\\": \\\"${username}\\\", \\\"password\\\": \\\"$PASSWORD\\\"}]\" -i /var/lib/AdGuardHome/AdGuardHome.yaml || true'"
-    ];
+     systemd.services.adguardhome.preStart = lib.mkAfter ''
+        PASSWORD=$(cat ${config.sops.secrets.adguardhome-password.path})
+        ${pkgs.yq-go}/bin/yq eval ". users = [{\"name\": \"${username}\", \"password\": \"$PASSWORD\"}]" -i /var/lib/AdGuardHome/AdGuardHome.yaml
+    '';
 }
