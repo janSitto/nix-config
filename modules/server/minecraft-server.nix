822432a1e1c9d9307dd958b866938743e4fd2801
janSitto <jvtrschmitt404@gmail.com>
2025-12-26 14:12:15 -0300

    minecraft-server

--- modules/server/minecraft-server.nix
+++ modules/server/minecraft-server.nix
@@ -1,4 +1,16 @@
-{config, pkgs, ...}: {
+{config, pkgs, ...}: 
+let
+  serverIcon = pkgs.runCommand "server-icon.png" {
+    nativeBuildInputs = [ pkgs.imagemagick ];
+    src = pkgs.fetchurl {
+      url = "https://minecraft.wiki/images/Book_and_Quill_JE2_BE2.png";
+      sha256 = "0a3yb9wkqhmanm4zwz2bpgdl2aa8x7gd44wajl3ijrk97d0h8n92";
+    };
+  } ''
+    convert $src -resize 64x64! $out
+  '';
+in
+{
     services.minecraft-server = {
         enable = true;
         eula = true;
@@ -22,12 +34,10 @@
             level-name = "Comuna dos Manos";
             level-seed = "Comuna dos Manos";
         };
-        extraFiles = {
-            "server-icon.png" = pkgs.fetchurl {
-                url = "https://minecraft.wiki/images/Book_and_Quill_JE2_BE2.png";
-                sha256 = "0a3yb9wkqhmanm4zwz2bpgdl2aa8x7gd44wajl3ijrk97d0h8n92";
-            };
-        };
+        dataDir = "/var/lib/minecraft";
         jvmOpts = "-Xms2048M -Xmx4096M";
     };
+    systemd.tmpfiles.rules = [
+        "L+ /var/lib/minecraft/server-icon.png - - - - ${serverIcon}"
+    ];
 }
