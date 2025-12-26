e323fb27a11ef3f43c89bb083ecaea584b29346d
janSitto <jvtrschmitt404@gmail.com>
2025-12-26 13:57:57 -0300

    minecraft-server

--- modules/server/minecraft-server.nix
+++ modules/server/minecraft-server.nix
@@ -14,13 +14,19 @@
             difficulty = 3;
             gamemode = 0;
             max-players = 10;
-            motd = "Comuna dos Manos";
+            motd = "Servidor NixOS de JVS";
             white-list = false;
             allow-cheats = false;
             enforce-secure-profile = false;
             force-gamemode = true;
             level-name = "Comuna dos Manos";
             level-seed = "Comuna dos Manos";
+        };
+        extraFiles = {
+            "server-icon.png" = pkgs.fetchurl {
+                url = "https://minecraft.wiki/images/Book_and_Quill_JE2_BE2.png";
+                sha256 = "0a3yb9wkqhmanm4zwz2bpgdl2aa8x7gd44wajl3ijrk97d0h8n92";
+            };
         };
         jvmOpts = "-Xms2048M -Xmx4096M";
     };
