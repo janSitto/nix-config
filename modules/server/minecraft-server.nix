7c891fd74d709dd79a9f8e74dfffba5e80afd4a2
janSitto <jvtrschmitt404@gmail.com>
2025-12-27 13:59:26 -0300

    minecraft-server

--- modules/server/minecraft-server.nix
+++ modules/server/minecraft-server.nix
@@ -28,21 +28,28 @@
         jvmOpts = "-Xms2048M -Xmx4096M";
     };
     environment.systemPackages = with pkgs; [ screen ];
-    systemd. services.minecraft-server.serviceConfig = {
-        Type = pkgs.lib.mkForce "forking";
-        ExecStart = pkgs.lib.mkForce ''
-            ${pkgs.screen}/bin/screen -DmS minecraft ${pkgs.bash}/bin/bash -c "cd /var/lib/minecraft && ${pkgs.jdk17}/bin/java ${config.services.minecraft-server.jvmOpts} -jar ${pkgs.papermc}/lib/papermc/papermc.jar nogui"
-        '';
-        ExecStop = pkgs.lib. mkForce ''
-            ${pkgs.screen}/bin/screen -S minecraft -X stuff 'stop^M'
+    systemd.sockets.minecraft-server.enable = false;
+    systemd.services.minecraft-server = {
+        requires = pkgs.lib.mkForce [];
+        after = pkgs.lib.mkForce [ "network.target" ];
+
+        serviceConfig = {
+            Type = pkgs.lib.mkForce "forking";
+            ExecStart = pkgs.lib. mkForce ''
+                ${pkgs.screen}/bin/screen -DmS minecraft ${pkgs. bash}/bin/bash -c "cd /var/lib/minecraft && ${pkgs.jdk17}/bin/java ${config.services. minecraft-server.jvmOpts} -jar ${pkgs. papermc}/lib/papermc/papermc.jar nogui"
+            '';
+            ExecStop = pkgs.lib. mkForce ''
+                ${pkgs.screen}/bin/screen -S minecraft -X stuff 'stop^M'
+            '';
+        };
+        
+        preStart = ''
+            cp -f ${pkgs.runCommand "icon.png" { nativeBuildInputs = [ pkgs.imagemagick ]; } ''
+                convert ${pkgs. fetchurl {
+                url = "https://minecraft.wiki/images/Book_and_Quill_JE2_BE2.png";
+                sha256 = "0a3yb9wkqhmanm4zwz2bpgdl2aa8x7gd44wajl3ijrk97d0h8n92";
+                }} -resize 64x64! $out
+            ''} /var/lib/minecraft/server-icon.png
         '';
     };
-    systemd.services.minecraft-server. preStart = ''
-        cp -f ${pkgs.runCommand "icon.png" { nativeBuildInputs = [ pkgs.imagemagick ]; } ''
-            convert ${pkgs. fetchurl {
-            url = "https://minecraft.wiki/images/Book_and_Quill_JE2_BE2.png";
-            sha256 = "0a3yb9wkqhmanm4zwz2bpgdl2aa8x7gd44wajl3ijrk97d0h8n92";
-            }} -resize 64x64! $out
-        ''} /var/lib/minecraft/server-icon.png
-    '';
 }
