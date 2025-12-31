3d437988f8b0b0f2d450b01fc9ed491b6217f7b3
janSitto <jvtrschmitt404@gmail.com>
2025-12-31 13:08:35 -0300

    minecraft-server plugins

--- modules/server/minecraft-server.nix
+++ modules/server/minecraft-server.nix
@@ -30,14 +30,24 @@
     environment.systemPackages = with pkgs; [ wget ];
     systemd.services.minecraft-server.preStart = ''
         cp -f ${pkgs.runCommand "icon.png" { nativeBuildInputs = [ pkgs.imagemagick ]; } ''
-            convert ${pkgs. fetchurl {
+            convert ${pkgs.fetchurl {
             url = "https://minecraft.wiki/images/Book_and_Quill_JE2_BE2.png";
             sha256 = "0a3yb9wkqhmanm4zwz2bpgdl2aa8x7gd44wajl3ijrk97d0h8n92";
             }} -resize 64x64! $out
         ''} /var/lib/minecraft/server-icon.png
 
         mkdir -p /var/lib/minecraft/plugins
-        
+       
+        if [ ! -f /var/lib/minecraft/plugins/minimotd-paper-2.2.2.jar ]; then
+        ${pkgs.curl}/bincurl -L -o /var/lib/minecraft/plugins/minimotd-paper-2.2.2.jar \
+            "https://hangarcdn.papermc.io/plugins/jmp/MiniMOTD/versions/2.2.2/PAPER/minimotd-paper-2.2.2.jar"
+        fi
+
+        if [ ! -f /var/lib/minecraft/plugins/tabtps-paper-1.3.30.jar ]; then
+        ${pkgs.curl}/bin/curl -L -o /var/lib/minecraft/plugins/tabtps-paper-1.3.30.jar \
+            "https://hangarcdn.papermc.io/plugins/jmp/TabTPS/versions/1.3.30/PAPER/tabtps-paper-1.3.30.jar"
+        fi
+
         if [ ! -f /var/lib/minecraft/plugins/Orebfuscator.jar ]; then
         ${pkgs.curl}/bin/curl -L -o /var/lib/minecraft/plugins/Orebfuscator.jar \
             "https://github.com/ImDaniX/Orebfuscator/releases/download/v5.4.1/Orebfuscator-5.4.1.jar"
