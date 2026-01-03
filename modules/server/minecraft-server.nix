0ddc17be525c7a8f2ba38b6d9d091da939522a45
janSitto <jvtrschmitt404@gmail.com>
2026-01-03 17:29:03 -0300

    minecraft-server

--- modules/server/minecraft-server.nix
+++ modules/server/minecraft-server.nix
@@ -100,7 +100,7 @@
         if [ ! -f /var/lib/minecraft/plugins/EssentialsX-2.22.0-dev+49-000e6c5.jar ]; then
         ${pkgs.curl}/bin/curl -L -o /var/lib/minecraft/plugins/EssentialsX-2.22.0-dev+49-000e6c5.jar \
             "https://ci.ender.zone/job/EssentialsX/lastSuccessfulBuild/artifact/jars/EssentialsX-2.22.0-dev+49-000e6c5.jar"
-        fi
+        fi 
 
         # Miscellaneous plugins
         
@@ -122,11 +122,6 @@
         if [ ! -f /var/lib/minecraft/plugins/voicechat-bukkit-2.6.7.jar ]; then
         ${pkgs.curl}/bin/curl -L -o /var/lib/minecraft/plugins/voicechat-bukkit-2.6.7.jar \
             "https://hangarcdn.papermc.io/plugins/henkelmax/SimpleVoiceChat/versions/bukkit-2.6.7/PAPER/voicechat-bukkit-2.6.7.jar"
-        fi
-
-        if [ ! -f /var/lib/minecraft/plugins/DistantHorizonsSupport-0.12.0.jar ]; then
-        ${pkgs.curl}/bin/curl -L -o /var/lib/minecraft/plugins/DistantHorizonsSupport-0.12.0.jar \
-            "https://cdn.modrinth.com/data/IjY7seTG/versions/FC75foTg/DistantHorizonsSupport-0.12.0.jar"
         fi
 
     '';
