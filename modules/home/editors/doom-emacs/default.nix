53ba2bd77080d52ba5b2c1ac74a1b9c22a7f78f4
janSitto <jvtrschmitt404@gmail.com>
2026-01-21 14:39:50 -0300

    doom-emacs

--- modules/home/editors/doom-emacs/default.nix
+++ modules/home/editors/doom-emacs/default.nix
@@ -9,7 +9,7 @@
       pkgs.treesit-grammars.with-all-grammars
       pkgs.emacs.pkgs.lsp-bridge
     ];
-    doomDir = ./doom.d;
+    doomDir = ./doom;
     tangleArgs = "--all config.org";
     experimentalFetchTree = true;
 
